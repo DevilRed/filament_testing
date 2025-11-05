6bbcc7644342ff802549cbb6da461711f2f1fb7f
Christian Quispe Rojas <cquispe@develoop.net>
2025-11-05 08:30:36 -0400

    add form validations

--- app/Filament/Resources/Employees/Schemas/EmployeeForm.php
+++ app/Filament/Resources/Employees/Schemas/EmployeeForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Employees\Schemas;
 
+use App\Models\Employee;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
@@ -11,12 +12,44 @@ class EmployeeForm
     {
         return $schema
             ->components([
-                TextInput::make('first_name'),
-                TextInput::make('last_name'),
-                TextInput::make('email'),
-                TextInput::make('phone'),
-                TextInput::make('position'),
-                TextInput::make('salary'),
+                TextInput::make('first_name')
+                        ->label('First Name')
+                        ->required()
+                        ->minLength(2)
+                        ->maxLength(50),
+                TextInput::make('last_name')
+                        ->label('Last Name')
+                        ->required()
+                        ->minLength(2)
+                        ->maxLength(50),
+                TextInput::make('email')
+                        ->label('Email')
+                        ->required()
+                        ->email()
+                        ->maxLength(150)
+                        ->unique(Employee::class, 'email', ignoreRecord: true),
+                TextInput::make('phone')
+                        ->label('Phone Number')
+                        ->required()
+                        ->tel()
+                        ->unique(Employee::class, 'phone', ignoreRecord: true)
+                        ->rules(['required', 'digits:10', 'numeric'])
+                        ->helperText('Enter a 10 digit phone number (numbers only, no spaces or symbols)')
+                        ->validationAttribute('phone_number'),
+                TextInput::make('position')
+                        ->label('Job Title')
+                        ->required()
+                        ->minLength(4)
+                        ->maxLength(150),
+                TextInput::make('salary')
+                        ->label('Salary')
+                        ->required()
+                        ->numeric()
+                        ->minValue(1000)
+                        ->maxValue(9999.99)
+                        ->step(0.01)
+                        ->placeholder('5000.00')
+                        ->helperText('Enter a salary between 1000.00 and 9999.99')
             ]);
     }
 }
