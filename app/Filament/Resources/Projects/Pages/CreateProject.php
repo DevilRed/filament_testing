5bf016fd727d3bd081707a5c0e6df04528ed0009
Christian Quispe Rojas <cquispe@develoop.net>
2025-11-05 12:21:12 -0400

    disable create another button on create project page

--- app/Filament/Resources/Projects/Pages/CreateProject.php
+++ app/Filament/Resources/Projects/Pages/CreateProject.php
@@ -8,4 +8,5 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateProject extends CreateRecord
 {
     protected static string $resource = ProjectResource::class;
+    protected static bool $canCreateAnother = false;
 }
