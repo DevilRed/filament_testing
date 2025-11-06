c84ae34ffb826719eb435fc3be34ff28a85675bf
Christian Quispe Rojas <cquispe@develoop.net>
2025-11-06 12:51:31 -0400

    update ListEmployess page, add property and set observer

--- app/Filament/Resources/Employees/Pages/ListEmployees.php
+++ app/Filament/Resources/Employees/Pages/ListEmployees.php
@@ -5,10 +5,12 @@ namespace App\Filament\Resources\Employees\Pages;
 use App\Filament\Resources\Employees\EmployeeResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Livewire\Attributes\On;
 
 class ListEmployees extends ListRecords
 {
     protected static string $resource = EmployeeResource::class;
+    public $selectedProject = null;
 
     protected static ?string $title = 'All Employees';
     protected ?string $heading = 'Employee List';
@@ -19,5 +21,14 @@ class ListEmployees extends ListRecords
             CreateAction::make()
                 ->createAnother(false),
         ];
+    }
+
+    #[On('project-selected')]
+    public function updateProjectFilter($projectId)
+    {
+        $this->selectedProject = $projectId;
+
+        // Reset the table to apply the new filter
+        $this->resetTable();
     }
 }
