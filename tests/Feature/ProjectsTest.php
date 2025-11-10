1dde7117acd6cd848e3ae9b9df092487e7897780
Christian Quispe Rojas <cquispe@develoop.net>
2025-11-10 10:10:36 -0400

    remove default tests and add more cases to test crud functionality

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -1,14 +1,30 @@
 <?php
 
+use App\Filament\Resources\Projects\Pages\CreateProject;
+use App\Filament\Resources\Projects\Pages\EditProject;
 use App\Filament\Resources\Projects\Pages\ListProjects;
 use Livewire\Livewire;
 use App\Models\Project;
 
-it('has projects page', function () {
+use function Pest\Livewire\livewire;
+
+it('can render projects list page', function () {
     $projects = Project::factory()
         ->count(3)
         ->create();
     Livewire::test(ListProjects::class)->assertCanSeeTableRecords(
         $projects
-    );
+    )
+    ->assertSuccessful();
+});
+
+it('can render the create page', function() {
+    livewire(CreateProject::class)
+    ->assertSuccessful();
+});
+
+it('can render the edit page', function() {
+    $project = Project::factory()->create();
+    livewire(EditProject::class, ['record' => $project->id])
+        ->assertSuccessful();
 });
