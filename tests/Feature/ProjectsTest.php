2ba9340d77454d8a96c07643d5f9daaa72879592
Christian Quispe Rojas <cquispe@develoop.net>
2025-11-10 11:03:52 -0400

    add test for creating a project

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -28,3 +28,20 @@ it('can render the edit page', function() {
     livewire(EditProject::class, ['record' => $project->id])
         ->assertSuccessful();
 });
+
+it('can create a project', function () {
+    // Use factory to produce a valid payload that matches the app expectations
+    $newProject = Project::factory()->make()->toArray();
+    livewire(CreateProject::class)
+        ->fillForm($newProject)
+        ->call('create')
+        ->assertHasNoFormErrors()
+        ->assertNotified()
+        ->assertRedirect();
+
+    $this->assertDatabaseHas('projects', [
+        'name' => $newProject['name'],
+        'description' => $newProject['description'],
+        'status' => $newProject['status'],
+    ]);
+});
